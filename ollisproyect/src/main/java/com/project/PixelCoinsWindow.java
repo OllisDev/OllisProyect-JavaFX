474006c77fe90559ce28a6b793dfd18d165ac0ba
OllisDev <ikermagro@outlook.es>
2025-03-12 17:34:07 +0100

    12/03: Configuration left BorderPane

--- ollisproyect/src/main/java/com/project/PixelCoinsWindow.java
+++ ollisproyect/src/main/java/com/project/PixelCoinsWindow.java
@@ -1,30 +1,43 @@
 package com.project;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class PixelCoinsWindow extends Application {
 
     @Override
     public void start(Stage pixelCoinWindow) {
-        BorderPane bpButtons = new BorderPane();
         Button btnAccount = new Button("Cuenta");
-        HBox layoutAccount = new HBox(10);
-        layoutAccount.getChildren().addAll(btnAccount);
+        Button btnLaunch = new Button("Lanzador");
+        Button btnShop = new Button("Tienda");
 
-        VBox layoutButtons = new VBox();
-        layoutButtons.getChildren().addAll(layoutAccount);
-        bpButtons.setLeft(layoutButtons);
-        bpButtons.setAlignment(layoutButtons, Pos.CENTER);
+        StackPane topPane = new StackPane(btnAccount);
+        StackPane centerPane = new StackPane(btnLaunch);
+        StackPane bottomPane = new StackPane(btnShop);
 
-        Scene scene = new Scene(bpButtons, 800, 800);
+        BorderPane.setMargin(topPane, new Insets(20, 10, 10, 10));  // Arriba
+        BorderPane.setMargin(bottomPane, new Insets(10, 10, 20, 10)); // Abajo
+        BorderPane.setMargin(centerPane, new Insets(10, 10, 10, 10)); // Espaciado general
 
+        BorderPane leftPane = new BorderPane();
+        leftPane.getStyleClass().add("border-pane");
+
+        leftPane.setTop(topPane);
+        leftPane.setCenter(centerPane);
+        leftPane.setBottom(bottomPane);
+
+        leftPane.setPrefWidth(200);
+
+        BorderPane mainPane = new BorderPane();
+        mainPane.setLeft(leftPane);
+
+        Scene scene = new Scene(mainPane, 1080, 920);
+        scene.getStylesheets().add(getClass().getResource("styles/pixelCoinsWindow.css").toExternalForm());
         pixelCoinWindow.setScene(scene);
         pixelCoinWindow.setTitle("PixelCoinsLauncher");
         pixelCoinWindow.show();
